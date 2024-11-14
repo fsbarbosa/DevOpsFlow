955f29ce73dde16822a1ec9752d969feb924548b
fsbarbosa <6672383+fsbarbosa@users.noreply.github.com>
2024-11-14 01:03:21 +0100

    advance the readability

--- frontend/index.js
+++ frontend/index.js
@@ -3,17 +3,24 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import App from './App';
 import PageNotFound from './PageNotFound';
+
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
+
+const Routes = () => (
+  <Switch>
+    <Route exact path="/" component={App} />
+    <Route component={PageNotFound} />
+  </Switch>
+);
+
 const Root = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={App} />
-        <Route component={PageNotFound} />
-      </Switch>
+      <Routes />
     </Router>
   );
 };
+
 ReactDOM.render(<Root />, document.getElementById('root'));
